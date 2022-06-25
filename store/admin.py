20c7bd40e4e0f19ba09a38a825d696fe9c7f5b96
kairatGroupd <80115640+kairatGroupd@users.noreply.github.com>
2022-06-25 11:30:30 +0300

    1

--- store/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .views import *
-# Register your models here.
-
-admin.site.register(Product)
-# admin.site.register(Transaction)
-# admin.site.register(Transfer)
-
